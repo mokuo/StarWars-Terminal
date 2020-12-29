d36c3b6771738ebbf7968fdd57e01f5b2c14b1da
mokuo <tennis10988.yk@gmail.com>
2020-12-29 19:52:37 +0900

    iTerm2 の Python API で背景画像を設定できる

--- starwars/starwars.go
+++ starwars/starwars.go
@@ -4,31 +4,27 @@ import (
 	"log"
 	"os"
 	"os/exec"
-	"path/filepath"
 
-	"github.com/MakeNowJust/heredoc/v2"
+	"github.com/mokuo/starwars-terminal/starwars/terminal"
 	"github.com/urfave/cli/v2"
 )
 
+// Terminal インターフェース
+type Terminal interface {
+	Setup()
+	Cmd() string
+	Args() []string
+}
+
 func main() {
-	p, e := filepath.Abs("./images/r2d2.png")
-	if e != nil {
-		log.Fatal(e)
-	}
-	script := heredoc.Docf(`
-		tell application "iTerm"
-			tell current session of current window
-				set background image to "%s"
-				set transparency to 0.5
-			end tell
-		end tell
-	`, p)
+	terminal := terminal.NewIterm2()
+	terminal.Setup()
 
 	app := cli.NewApp()
 	app.Name = "StarWars Terminal"
 	app.Usage = "May the Force be with you."
 	app.Action = func(c *cli.Context) error {
-		err := exec.Command("osascript", "-e", script).Run()
+		err := exec.Command(terminal.Cmd(), terminal.Args()...).Run()
 		if err != nil {
 			return err
 		}
